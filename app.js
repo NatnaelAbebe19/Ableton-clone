d46b238571117c1e3b936369215eefb5f156d8c6
Natnael Abebe <natnaelabebe1994@gmail.com>
2023-12-24 00:19:56 +0300

    the hero and the sticky navbar are now done

---
+++ app.js
@@ -0,0 +1,12 @@
+// const navBar2 = document.getElementById("navbar--2");
+// let scrollPixels = 200;
+
+// window.addEventListener("scroll", function () {
+//   let scrollPosition = window.pageYOffset;
+
+//   if (scrollPosition > scrollPixels) {
+//     navBar2.style.display = "none";
+//   } else {
+//     navBar2.style.display = "block";
+//   }
+// });
